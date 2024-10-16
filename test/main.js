520d877d85f3e8b0d771d4cf81d25f9ee562ddab
s26016041 <s26016041@gmail.com>
2024-10-16 14:51:03 +0800

    Organize files

--- test/main.js
+++ test/main.js
@@ -11,40 +11,40 @@ const params = {
   toAll: 0,
 };
 
-// 发送 POST 请求
-axios.post('https://maplestory.beanfun.com/main?handler=BulletinProxy', qs.stringify(params), {
-  headers: {
-    'X-CSRF-Token': 'CfDJ8N5WPr6Y241KvDQ5Q5Dp0OK95QJo2Y21AIhPPU2_FXC9vfH_M5no-4OkMVM3rTIuC_7bGflQgcflL4o7bQjiAY3UCfsh4SUds32R0xW_0pyUMtmHxlTr7PUPkFtwkSZmsCyJbtvZDat76xBluTIzVpA',
-    'Cookie': '.AspNetCore.Antiforgery.SVSW8BVrGmw=CfDJ8N5WPr6Y241KvDQ5Q5Dp0OKNSYuDDnuUS2eR1AX0e2B5NHm0xxGCDBZmtSFWEiJfMFlC_070BRI3Cctk94-VT1LqgLoMkcJ78yf8xuv7G8PoA6Iwe_4DLLTp4R7uks3BzqihRiwkRqHmontfMlyeOKE; path=/; samesite=strict; httponly'
-  },
-})
-.then(response => {
-  console.log('成功响应:', response.data.data.myDataSet.table);
-})
-.catch(error => {
-  console.error('请求错误:', error);
-});
+let cookiesData, CSRFToken;
 
+axios.get('https://maplestory.beanfun.com/Main')
+  .then(response => {
+    // 从响应头提取 Set-Cookie
+    const cookies = response.headers['set-cookie'];
 
+    // 输出提取的 Cookie
+    console.log('提取的 Cookies:', cookies);
+    
+    // 使用 cheerio 加载 HTML
+    const $ = cheerio.load(response.data);
 
+    // 查找 __RequestVerificationToken 的值
+    const token = $('input[name="__RequestVerificationToken"]').val();
 
-// axios.get('https://maplestory.beanfun.com/Main')
-//   .then(response => {
-//     // 从响应头提取 Set-Cookie
-//     const cookies = response.headers['set-cookie'];
+    // 输出提取的 token
+    console.log('提取的 __RequestVerificationToken:', token);
+    
+    // 赋值给 CSRFToken 和 cookiesData
+    CSRFToken = token;
+    cookiesData = cookies.join('; '); // 确保 cookies 格式正确
 
-//     // 输出提取的 Cookie
-//     console.log('提取的 Cookies:', cookies);
-
-//     // 使用 cheerio 加载 HTML
-//     const $ = cheerio.load(response.data);
-
-//     // 查找 __RequestVerificationToken 的值
-//     const token = $('input[name="__RequestVerificationToken"]').val();
-
-//     // 输出提取的 token
-//     console.log('提取的 __RequestVerificationToken:', token);
-//   })
-//   .catch(error => {
-//     console.error('请求错误:', error);
-//   });
+    // 发送 POST 请求
+    return axios.post('https://maplestory.beanfun.com/main?handler=BulletinProxy', qs.stringify(params), {
+      headers: {
+        'X-CSRF-Token': CSRFToken,
+        'Cookie': cookiesData
+      },
+    });
+  })
+  .then(response => {
+    console.log('成功响应:', response.data.data.myDataSet.table);
+  })
+  .catch(error => {
+    console.error('请求错误:', error);
+  });
